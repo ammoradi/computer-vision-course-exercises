26e78d2449fc517b706dabfee3788f902419af24
امــیـرمــحـمــد <amirmohammad.moradi@gmail.com>
2018-10-20 04:05:37 +0330

    working on HW1

--- HW1/app.js
+++ HW1/app.js
@@ -1,12 +1,33 @@
+// Image upload handlings
 let imgElement = document.getElementById('imageSrc');
 let inputElement = document.getElementById('fileInput');
 inputElement.addEventListener('change', (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
+// After image upload
 imgElement.onload = function () {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
+    let src = cv.imread(imgElement);
+    let rgbaPlanes = new cv.MatVector();
+    // Split the Mat
+    cv.split(src, rgbaPlanes);
+    // Get channels
+    let R = rgbaPlanes.get(0);
+    let G = rgbaPlanes.get(1);
+    let B = rgbaPlanes.get(2);
+    // Get channels lightings
+    console.log(cv.mean(R)[0].toFixed(2))
+    console.log(cv.mean(G)[0].toFixed(2))
+    console.log(cv.mean(B)[0].toFixed(2))
+    // Show Channels
+    cv.imshow('redChannel',R);
+    cv.imshow('greenChannel',G);
+    cv.imshow('blueChannel',B);
+    // Make memory free
+    src.delete();
+    rgbaPlanes.delete();
+    R.delete();
+    G.delete();
+    B.delete();
 };
 
 function onOpenCvReady() {
---
+++ HW1/doc.md
@@ -0,0 +1,10 @@
+#What the program does?:
+=============
+* read image from computer
+* show image's color channels
+* write image's average lighting degree
+=============
+#How to run?:
+```
+open index.html with a browser
+```
