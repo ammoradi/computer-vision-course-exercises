b5ca07a4a5218975c832366999180a053e100abf
امــیـرمــحـمــد <amirmohammad.moradi@gmail.com>
2018-10-22 00:09:20 +0330

    features completed

--- HW1/app.js
+++ HW1/app.js
@@ -4,24 +4,31 @@ let inputElement = document.getElementById('fileInput');
 inputElement.addEventListener('change', (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
+
 // After image upload
 imgElement.onload = function () {
+    // initialize variables
     let src = cv.imread(imgElement);
     let rgbaPlanes = new cv.MatVector();
+
     // Split the Mat
     cv.split(src, rgbaPlanes);
+
     // Get channels
     let R = rgbaPlanes.get(0);
     let G = rgbaPlanes.get(1);
     let B = rgbaPlanes.get(2);
+
     // Get channels lightings
-    console.log(cv.mean(R)[0].toFixed(2))
-    console.log(cv.mean(G)[0].toFixed(2))
-    console.log(cv.mean(B)[0].toFixed(2))
+    document.getElementById('redChannelMean').innerHTML = cv.mean(R)[0].toFixed(2);
+    document.getElementById('greenChannelMean').innerHTML = cv.mean(G)[0].toFixed(2);
+    document.getElementById('blueChannelMean').innerHTML = cv.mean(B)[0].toFixed(2);
+
     // Show Channels
     cv.imshow('redChannel',R);
     cv.imshow('greenChannel',G);
     cv.imshow('blueChannel',B);
+
     // Make memory free
     src.delete();
     rgbaPlanes.delete();
